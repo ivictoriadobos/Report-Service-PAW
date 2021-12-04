737d2580c4b995708cb6965b9a2c9f8315cf0c18
Ioana-Victoria Dobos <ioana162@yahoo.com>
2021-12-05 01:40:48 +0200

    Initial commit

---
+++ src/test/java/paw/report/service/ApplicationTests.java
@@ -0,0 +1,13 @@
+package paw.report.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
